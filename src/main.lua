434065f3deb89c0a98df076418c99339c540cc62
maybe-adamant <maybe.adamant@gmail.com>
2026-04-07 18:52:01 -0400

    chore: align metadata fields

--- src/main.lua
+++ src/main.lua
@@ -64,9 +64,9 @@ public.definition = {
     id           = "RunModsNPCs",
     name         = "Run Modifiers: NPCs & Routing",
     category     = "Run Modifiers",
-    group        = "NPCs & Routing",
+    subgroup     = "NPCs & Routing",
     tooltip      = "Run modifier options for NPC spawns and routing.",
-    default      = true,
+    default      = dataDefaults.Enabled,
     affectsRunData = true,
 }
 
@@ -115,4 +115,3 @@ end)
 local uiCallback = lib.standaloneUI(public.definition, store)
 ---@diagnostic disable-next-line: redundant-parameter
 rom.gui.add_to_menu_bar(uiCallback)
-
